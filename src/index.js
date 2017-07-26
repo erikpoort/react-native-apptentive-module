dfe04f2b6f3f8ee4ce5d1307022e685242f0f6b5
Erik Poort <erik@mediamonks.com>
2017-07-26 15:08:45 -0300

    Add documentation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,54 @@
 import { NativeModules } from 'react-native';
 const { ApptentiveModule } = NativeModules;
 
-console.log('ApptentiveModule', ApptentiveModule)
+/**
+ * Configure Apptentive using you key and signature.
+ * You can add an optional AppleID to redirect the user to the App Store
+ * @param appKey Found in Apptentive Dashboard
+ * @param appSignature Found in Apptentive Dashboard
+ * @param appleID optional Found in iTunesConnect
+ * @return Promise
+ */
+function registerWithAppKey(appKey, appSignature, appleID) {
+  return ApptentiveModule.registerWithAppKey(appKey, appSignature, appleID);
+}
 
-export default ApptentiveModule;
+/**
+ * Present the feedback view
+ * @return Promise
+ */
+function presentMessageCenterWithResolver() {
+  return ApptentiveModule.presentMessageCenterWithResolver();
+}
+
+/**
+ * Present the feedback view with custom data
+ * @param customData optional
+ * @return Promise
+ */
+function presentMessageCenterWithCustomData(customData) {
+  return ApptentiveModule.presentMessageCenterWithCustomData(customData);
+}
+
+/**
+ * Log an event to apptentive
+ * @param event
+ * @return Promise
+ */
+function engageEvent(event) {
+  return ApptentiveModule.engageEvent(event);
+}
+
+/**
+ * Log an event with custom data to apptentive
+ * @param event
+ * @param customData optional
+ * @return Promise
+ */
+function engageEventWithCustomData(event, customData) {
+  return ApptentiveModule.engageEventWithCustomData(event, customData);
+}
+
+module.exports = {
+  registerWithAppKey,
+}
