2ee84e03dedeefb9cd50eb008c761bf3a15b4478
Erik Poort <erik@mediamonks.com>
2017-07-26 11:36:33 -0300

    Add first iOS classes

---
+++ ios/react-native-apptentive-module/ApptentiveModule.m
@@ -0,0 +1,13 @@
+//
+//  ApptentiveModule.m
+//  react-native-apptentive-module
+//
+//  Created by Erik Poort on 26/07/2017.
+//  Copyright (c) 2017 MediaMonks. All rights reserved.
+//
+
+#import "ApptentiveModule.h"
+
+@implementation ApptentiveModule
+
+@end
